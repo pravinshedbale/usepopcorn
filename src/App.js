9460e1996fefa75c5334df69e5c98815f66462c1
pravinshedbale <pravin.shedbale@outlook.com>
2023-08-25 13:31:20 +0530

    warnings removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Logo from "./components/Logo";
